456d282de3fc7801459730acff79552a24b7d33e
Simon <78990353+simonfuture2@users.noreply.github.com>
2022-09-03 14:38:56 -0400

    Update 1_accounts.js

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -3,7 +3,7 @@ const { ethers } = require("ethers");
 const INFURA_ID = ''
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
-const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+const address = '0x0e00A8B3a3f971aF0BD5E6024A321Cfa1778bAf2'
 
 const main = async () => {
     const balance = await provider.getBalance(address)
